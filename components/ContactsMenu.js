3c96f89ea885946916f05a09ac86d5d61108e09e
Satyajit <sahoosatyajit2801@gmail.com>
2024-03-15 00:36:52 +0530

    added contact menu page

--- components/ContactsMenu.js
+++ components/ContactsMenu.js
@@ -1,14 +1,88 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react';
+import AntDesign from "react-native-vector-icons/AntDesign";
+
+const contactsMenuButtons = [
+    {
+        type: "starred",
+        name: "Starred"
+    },
+    {
+        type: "contact",
+        name: "Satyajit Sahoo",
+        photo: require("../assets/client-1.jpg")
+    },
+    {
+        type: "contact",
+        name: "John Doe",
+        photo: require("../assets/client-2.jpg")
+    },
+    {
+        type: "contact",
+        name: "Jane Doe",
+        photo: require("../assets/client-3.jpg")
+    }
+]
 
 const ContactsMenu = () => {
     return (
-        <View>
-            <Text>ContactsMenu</Text>
-        </View>
+        <View style={styles.container}>
+            {
+                contactsMenuButtons.map((contact, index) => {
+                    {/* contact container */ }
+                    return (
+                        <View style={styles.row} key={index}>
+                            {/* Image */}
+                            {contact.type === "starred" ? (
+
+                                < View style={styles.starredIcon} >
+                                    <AntDesign name='star' size={30} color={"#efefef"} />
+                                </View>
+                            ) : (
+                                <Image
+                                    source={contact.photo}
+                                    style={styles.image}
+                                />
+                            )}
+                            {/* Text */}
+                            <Text style={styles.text}>
+                                {contact.name}
+                            </Text>
+                        </View >
+                    )
+                })
+            }
+        </View >
     )
 }
 
 export default ContactsMenu
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    row: {
+        flexDirection: 'row',
+        marginTop: 20,
+        alignItems: 'center',
+    },
+    starredIcon: {
+        backgroundColor: "#333333",
+        width: 55,
+        height: 55,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20
+    },
+    text: {
+        color: 'white',
+        paddingLeft: 15,
+        fontSize: 18,
+    },
+    image: {
+        width: 55,
+        height: 55,
+        borderRadius: 20,
+    }
+})
