4e7ff4a6b2f370f7c4efd0731b3039e53c8485d1
Satyajit <sahoosatyajit2801@gmail.com>
2024-03-20 22:31:50 +0530

    added camera

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native'
 import React, { useEffect, useMemo, useState } from 'react'
 import StartMeeting from '../components/StartMeeting';
 import { initSocket } from '../socket';
 import { io } from "socket.io-client";
+import { Camera } from 'expo-camera';
 
 let socket;
 
@@ -10,22 +11,33 @@ const MeetingRoom = () => {
     const [name, setName] = useState();
     const [roomId, setRoomId] = useState();
     const [activeUsers, setActiveUsers] = useState();
+    const [startCamera, setStartCamera] = useState(false);
+
+    const startCameraHandler = async () => {
+        const { status } = await Camera.requestCameraPermissionsAsync();
+        if (status === "granted") {
+            setStartCamera(true);
+        } else {
+            Alert.alert("Access denied!");
+        }
+    }
 
     const joinRoom = () => {
+        startCameraHandler();
         socket.emit('join-room', { roomId: roomId, userName: name })
     }
 
     useEffect(() => {
-        socket = io("https://13a2-2401-4900-313a-3533-892f-7df8-b0b-e08a.ngrok-free.app")
+        socket = io("https://dd4b-2401-4900-74dd-f45f-d0fd-a6ff-91c4-1c19.ngrok-free.app")
         socket.on('connection', () => {
             console.log("connected")
         })
 
-        socket.on('all-users', users => {
-            console.log("Active users");
-            console.log(users)
-            setActiveUsers(users);
-        })
+        // socket.on('all-users', users => {
+        //     console.log("Active users");
+        //     console.log(users)
+        //     setActiveUsers(users);
+        // })
     }, [])
 
 
@@ -33,13 +45,22 @@ const MeetingRoom = () => {
 
     return (
         <View style={styles.container}>
-            <StartMeeting
-                name={name}
-                setName={setName}
-                roomId={roomId}
-                setRoomId={setRoomId}
-                joinRoom={joinRoom}
-            />
+            {startCamera ? (
+                <Camera
+                    type={"front"}
+                    style={{ width: "100%", height: 600 }}
+                >
+
+                </Camera>
+            ) : (
+                <StartMeeting
+                    name={name}
+                    setName={setName}
+                    roomId={roomId}
+                    setRoomId={setRoomId}
+                    joinRoom={joinRoom}
+                />
+            )}
         </View>
     )
 }
